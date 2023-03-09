c7c582244ef3a34dd7b445081bcbd19f7a664649
Michał Gawroński <mic.gawronski@gmail.com>
2023-03-09 12:38:05 +0100

    basic add skeleton, add notes file

--- main-folder/app.js
+++ main-folder/app.js
@@ -1,1 +1,7 @@
-console.log("Anything, just a test");
+function add(n1, n2) {
+    return n1 + n2;
+}
+var number1 = "52";
+var number2 = 2.8;
+var result = add(number1, number2);
+console.log(result);
---
+++ main-folder/notes.md
@@ -0,0 +1,2 @@
+Section 1 part 9
+npm init - install for live server // works anyway i didint install it.
