d7dd2c204fd0932fb4db110edf794ead153537a4
Michał Gawroński <mic.gawronski@gmail.com>
2023-03-13 11:11:06 +0100

    Enum intro.

--- main-folder/app.js
+++ main-folder/app.js
@@ -1,8 +1,19 @@
+// const person: {
+//   name: string;
+//   age: number;
+//   hobby: string[];
+//   role: [number, string]; // tuple type
+// } = {
+//   name: "Michael",
+//   age: 28,
+//   hobby: ["Psychology", "Cooking"],
+//   role: [2, "singer"],
+// };
 var person = {
     name: "Michael",
     age: 28,
     hobby: ["Psychology", "Cooking"],
-    role: [2, "singer"]
+    role: "Read Only User"
 };
 // console.log(person.name);
 var favoriteActivities = ["Sports"];
@@ -11,3 +22,8 @@ for (var _i = 0, _a = person.hobby; _i < _a.length; _i++) {
     console.log(hobby.toLocaleUpperCase());
     // console.log(hobby.map()) typescript knows what kind of methods are allowed.
 }
+if (person.role === "READ_ONLY_USER") {
+    // never works, diffrences in typing method. Enum intro.
+    console.log("anything");
+}
+console.log(person.role);
--- main-folder/app.ts
+++ main-folder/app.ts
@@ -1,13 +1,19 @@
-const person: {
-  name: string;
-  age: number;
-  hobby: string[];
-  role: [number, string]; // tuple type
-} = {
+// const person: {
+//   name: string;
+//   age: number;
+//   hobby: string[];
+//   role: [number, string]; // tuple type
+// } = {
+//   name: "Michael",
+//   age: 28,
+//   hobby: ["Psychology", "Cooking"],
+//   role: [2, "singer"],
+// };
+const person = {
   name: "Michael",
   age: 28,
   hobby: ["Psychology", "Cooking"],
-  role: [2, "singer"],
+  role: "Read Only User",
 };
 // console.log(person.name);
 
@@ -17,3 +23,8 @@ for (const hobby of person.hobby) {
   console.log(hobby.toLocaleUpperCase());
   // console.log(hobby.map()) typescript knows what kind of methods are allowed.
 }
+if (person.role === "READ_ONLY_USER") {
+  // never works, diffrences in typing method. Enum intro.
+  console.log("anything");
+}
+console.log(person.role);
